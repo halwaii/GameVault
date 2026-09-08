5914a953f1e6673c4f911ed96bcdc993df2ce4d6
halwaii <dacron1005@gmail.com>
2026-09-08 21:15:28 +0530

    added addGame() function

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 // 1) module -> creating angular js module
 var app = angular.module("gameApp",[]);
 
+
+
 // 2) controller
 app.controller("Gamecontroller",function($scope){
     // scope works as bridge between JS and html
     // this creates <h2>{{message}}</h2> in html
-    $scope.message = "hie lol";
+    //$scope.message = "Welcome to GameVault";
+
+    // array of games and each game is object
+    // with name, genre, status, rating
+    $scope.games=[
+        {
+            name: "withcher 3",
+            genre: "RPG",
+            status: "playing",
+            rating: 10
+        },
+        {
+            name: "Sifu",
+            genre: "martial arts combat",
+            status: "completed",
+            rating: 9
+        },
+        {
+            name: "GTA 6",
+            genre: "action-adventure",
+            status: "wishlist",
+            rating: 10
+        }
+    ];
+
+    $scope.addGame = function(){
+    $scope.games.push({
+        name: $scope.newGame.name,
+        genre: $scope.newGame.genre,
+        status: $scope.newGame.status,
+        rating: $scope.newGame.rating
+    });
+    $scope.newGame = {};
+}
 });
